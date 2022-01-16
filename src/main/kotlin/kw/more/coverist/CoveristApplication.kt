cd62646765e0deb788f6c30968bf500641e3c883
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-01-17 04:18:16 +0900

    initial commit

---
+++ src/main/kotlin/kw/more/coverist/CoveristApplication.kt
@@ -0,0 +1,11 @@
+package kw.more.coverist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoveristApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoveristApplication>(*args)
+}
---
+++ src/test/kotlin/kw/more/coverist/CoveristApplicationTests.kt
@@ -0,0 +1,13 @@
+package kw.more.coverist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoveristApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
