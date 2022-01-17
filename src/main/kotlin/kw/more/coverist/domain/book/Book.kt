5fbcdc6f010b21743d56254fe1801e9792d1392a
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-01-17 21:10:27 +0900

    create book entity & repository

---
+++ src/main/kotlin/kw/more/coverist/domain/book/Book.kt
@@ -0,0 +1,30 @@
+package kw.more.coverist.domain.book
+
+import kw.more.coverist.domain.BaseTimeEntity
+import kw.more.coverist.domain.cover.Cover
+import javax.persistence.*
+
+@Entity
+class Book  (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    var id: Long? = null,
+
+    var title: String,
+
+    var author: String,
+
+    @Enumerated(EnumType.STRING)
+    var genre: Genre,
+
+    @ElementCollection
+    @CollectionTable(name = "tag", joinColumns = [JoinColumn(name = "book_id")])
+    @Column(name = "text")
+    var tags: Set<String>,
+
+    var publisher: String,
+
+    @OneToMany(mappedBy = "book")
+    var covers: MutableList<Cover> = mutableListOf<Cover>()
+) : BaseTimeEntity()
