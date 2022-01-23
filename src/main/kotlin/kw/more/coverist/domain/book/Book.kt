9981c8f7dff19b3bbc0cafe58a8ec2b01f15e952
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-01-23 23:49:31 +0900

    [impl] toResponseDto

--- src/main/kotlin/kw/more/coverist/domain/book/Book.kt
+++ src/main/kotlin/kw/more/coverist/domain/book/Book.kt
@@ -2,6 +2,7 @@ package kw.more.coverist.domain.book
 
 import kw.more.coverist.domain.BaseTimeEntity
 import kw.more.coverist.domain.cover.Cover
+import kw.more.coverist.web.dto.BookResponseDto
 import javax.persistence.*
 
 @Entity
@@ -28,4 +29,16 @@ class Book  (
     @OneToMany(mappedBy = "book")
     var covers: MutableList<Cover> = mutableListOf<Cover>()
 
-) : BaseTimeEntity()
+) : BaseTimeEntity() {
+    fun toResponseDto(): BookResponseDto {
+        return BookResponseDto(
+            id = id,
+            title = title,
+            author = author,
+            genre = genre.toString(),
+            tags = tags,
+            publisher = publisher,
+            covers = covers.map { it.id }
+        )
+    }
+}
