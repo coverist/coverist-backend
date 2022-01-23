f63e7af414eb263ee89e783d721ed2cf9fa7c03d
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-01-23 21:35:43 +0900

    [impl] entity -> responseDto convert

--- src/main/kotlin/kw/more/coverist/domain/cover/Cover.kt
+++ src/main/kotlin/kw/more/coverist/domain/cover/Cover.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import kw.more.coverist.domain.BaseTimeEntity
 import kw.more.coverist.domain.book.Book
+import kw.more.coverist.web.dto.CoverResponseDto
 import javax.persistence.*
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
@@ -20,4 +21,16 @@ class Cover (
 
        var url: String
 
-) : BaseTimeEntity()
+) : BaseTimeEntity() {
+       fun toResponseDto(): CoverResponseDto {
+              return CoverResponseDto(
+                     id = id,
+                     title = book.title,
+                     author = book.author,
+                     genre = book.genre.toString(),
+                     tags = book.tags,
+                     publisher = book.publisher,
+                     url = url
+              )
+       }
+}
