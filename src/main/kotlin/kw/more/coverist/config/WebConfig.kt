1916e79520929150d3bfd801a666eeb24a951868
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-03-02 02:14:25 +0900

    [fix] web config for CORS (for localhost test)

---
+++ src/main/kotlin/kw/more/coverist/config/WebConfig.kt
@@ -0,0 +1,16 @@
+package kw.more.coverist.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    val origin = "http://localhost"
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(origin)
+    }
+}
