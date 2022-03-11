738dae172bcb31d08453a3708d41d8e8308d281a
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-03-11 12:24:15 +0900

    [refactor] cover generation logic

--- src/main/kotlin/kw/more/coverist/service/cover/CoverService.kt
+++ src/main/kotlin/kw/more/coverist/service/cover/CoverService.kt
@@ -9,9 +9,10 @@ import kw.more.coverist.exception.custom.InvalidGenreException
 import kw.more.coverist.exception.custom.InvalidSubGenreException
 import kw.more.coverist.exception.custom.NonexistentBookException
 import kw.more.coverist.exception.custom.NonexistentCoverException
-import kw.more.coverist.web.dto.BookRequestDto
+import kw.more.coverist.util.AmazonS3Util
 import kw.more.coverist.web.dto.CoverResponseDto
-import kw.more.coverist.web.dto.CoverUrlResponseDto
+import kw.more.coverist.web.dto.CoverUrlsResponseDto
+import kw.more.coverist.web.vo.BookRequestVO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -24,63 +25,54 @@ class CoverService {
     @Autowired
     lateinit var coverRepository: CoverRepository
 
+    @Autowired
+    lateinit var amazonS3Util: AmazonS3Util
+
     @Transactional
-    fun genNewCover(book: BookRequestDto): List<CoverResponseDto> {
-        checkGenreValidation(book)
+    fun genNewCover(bookRequestVO: BookRequestVO): List<CoverResponseDto> {
+        checkGenreValidation(bookRequestVO)
 
-        val book = bookRepository.save(book.toEntity())
+        val publisherUrl = bookRequestVO.publisher?.let {
+            amazonS3Util.uploadFile(it, "publisher")
+        }
 
-        // 1. book 정보를 AI 서버로 전달 후
-        // 2. 생성된 cover 이미지가 AWS S3에 업로드 된 후
-        // 3. AI 서버가 본 서버로 S3 URL을 반환한다
+        val bookId = bookRepository.save(bookRequestVO.toEntity(publisherUrl)).id
 
-        val coverUrl = listOf(
-            "COVERIST_COVER_S3_URL_SAMPLE_1",
-            "COVERIST_COVER_S3_URL_SAMPLE_2",
-            "COVERIST_COVER_S3_URL_SAMPLE_3",
-            "COVERIST_COVER_S3_URL_SAMPLE_4"
-        )
+        return genBookCover(bookId!!)
+    }
 
-        val result = mutableListOf<CoverResponseDto>()
+    @Transactional
+    fun genBookCover(id: Long): List<CoverResponseDto> {
+        val book = bookRepository.findById(id).orElseThrow { NonexistentBookException() }
+
+        // 현재는 테스트를 위해 cover를 book의 publisher 이미지로 대체하여 반환
+        // FIXME: 테스트 종료 후 ?: "null" 지울 것
+        val coverUrl = List(4) { book.publisher ?: "null" }
+
+        val covers = mutableListOf<CoverResponseDto>()
 
         for (url in coverUrl) {
             val cover = coverRepository.save(Cover(book = book, url = url))
             book.covers.add(cover)
-            result.add(cover.toResponseDto())
+            covers.add(cover.toResponseDto())
         }
 
-        return result
-    }
-
-    @Transactional
-    fun genBookCover(id: Long): CoverResponseDto {
-        val book = bookRepository.findById(id).orElseThrow { NonexistentBookException() }
-
-        // 1. book 정보를 AI 서버로 전달 후
-        // 2. 생성된 cover 이미지가 AWS S3에 업로드 된 후
-        // 3. AI 서버가 본 서버로 S3 URL을 반환한다
-
-        val coverUrl = "COVERIST_COVER_S3_URL_SAMPLE"
-
-        val cover = coverRepository.save(Cover(book = book, url = coverUrl))
-        book.covers.add(cover)
-
-        return cover.toResponseDto()
+        return covers
     }
 
     @Transactional(readOnly = true)
-    fun findByBook(id: Long): CoverUrlResponseDto {
+    fun getCoverUrls(id: Long): CoverUrlsResponseDto {
         val covers = bookRepository.findById(id).orElseThrow { NonexistentBookException() }.covers
-        return CoverUrlResponseDto(covers.map { it.url })
+        return CoverUrlsResponseDto(covers.map { it.url })
     }
 
     @Transactional(readOnly = true)
-    fun findById(id: Long): CoverResponseDto {
+    fun getCover(id: Long): CoverResponseDto {
         return coverRepository.findById(id).orElseThrow { NonexistentCoverException() }.toResponseDto()
     }
 
-    fun checkGenreValidation(book: BookRequestDto) {
+    fun checkGenreValidation(book: BookRequestVO) {
         val genreId = GENRES.find { it.text == book.genre }?.id ?: throw InvalidGenreException()
-        SUB_GENRES[genreId]!!.find { it == book.subGenre } ?: throw InvalidSubGenreException()
+        SUB_GENRES[genreId]!!.find { it == book.sub_genre } ?: throw InvalidSubGenreException()
     }
 }
