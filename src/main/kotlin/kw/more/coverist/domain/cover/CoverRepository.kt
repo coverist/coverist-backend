b596ccdf51f1dd6c31fccf44097704f6b63d7689
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-03-16 06:23:53 +0900

    [refactor] remove unused import

--- src/main/kotlin/kw/more/coverist/domain/cover/CoverRepository.kt
+++ src/main/kotlin/kw/more/coverist/domain/cover/CoverRepository.kt
@@ -1,11 +1,9 @@
 package kw.more.coverist.domain.cover
 
-import kw.more.coverist.domain.book.Book
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 
-interface CoverRepository  : JpaRepository<Cover, Long> {
+interface CoverRepository : JpaRepository<Cover, Long> {
     @Query("SELECT c FROM cover c INNER JOIN c.book b WHERE b.id = :id", nativeQuery = true)
     fun findByBook(id: Long): List<Cover>
 }
