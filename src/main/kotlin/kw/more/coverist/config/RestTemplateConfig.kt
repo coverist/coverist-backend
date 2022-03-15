7f1e49ab2957798042efa4d1e5ec974c39b6071f
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-03-16 06:26:07 +0900

    [feat] RestTemplate config for Bean

---
+++ src/main/kotlin/kw/more/coverist/config/RestTemplateConfig.kt
@@ -0,0 +1,30 @@
+package kw.more.coverist.config
+
+import org.apache.http.impl.client.HttpClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import org.springframework.web.client.RestTemplate
+import java.security.KeyManagementException
+import java.security.NoSuchAlgorithmException
+import java.time.Duration
+
+@Configuration
+class RestTemplateConfig(
+    @Value("/")
+    val host: String
+) {
+    @Bean
+    @Throws(NoSuchAlgorithmException::class, KeyManagementException::class)
+    fun restTemplate(restTemplateBuilder: RestTemplateBuilder): RestTemplate {
+        val httpClient = HttpClientBuilder.create().setMaxConnTotal(50).setMaxConnPerRoute(20).build()
+        val factory = HttpComponentsClientHttpRequestFactory(httpClient)
+        
+        factory.setConnectTimeout(Duration.ofSeconds(3).toMillis().toInt())
+        factory.setReadTimeout(Duration.ofSeconds(10).toMillis().toInt())
+
+        return restTemplateBuilder.rootUri(host).requestFactory { factory }.build()
+    }
+}
